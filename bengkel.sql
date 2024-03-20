f9097acde2917e13b2121fe67f86f86c3a77dfd4
shofyyahcaca <shofyyahcaca@gmail.com>
2024-03-20 12:26:16 +0700

    update sql

---
+++ bengkel.sql
@@ -0,0 +1,10 @@
+create database bengkelku;
+
+CREATE TABLE bengkel ( 
+    id INTEGER NOT NULL, 
+    nama VARCHAR(100) NOT NULL, 
+    latitude FLOAT NOT NULL, 
+    longitude FLOAT NOT NULL, 
+    PRIMARY KEY (id) 
+    
+    );
