04b11f20a53155e5be28f59720e5b2d43c3a14be
shofyyahcaca <shofyyahcaca@gmail.com>
2024-04-03 02:01:49 +0700

    update database

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, request, render_template,jsonify
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///bengkelku.db' 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///bengkel.db' 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
